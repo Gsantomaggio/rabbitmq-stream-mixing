eadbfad17ca6c4d913017026aae242f3a56b6b34
Gabriele Santomaggio <G.santomaggio@gmail.com>
2026-09-22 15:40:33 +0200

    fix
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- k8s_setup/deploy_python_amqp_console.sh
+++ k8s_setup/deploy_python_amqp_console.sh
@@ -2,5 +2,5 @@ rm -rf rabbitmq-amqp-python-client
 git clone --branch v2_main https://github.com/rabbitmq/rabbitmq-amqp-python-client.git
 docker build -f rabbitmq-amqp-python-client/docs/examples/docker/Dockerfile -t python-amqp-reliable-client rabbitmq-amqp-python-client
 rm -rf rabbitmq-amqp-python-client
-docker tag python-amqp-console-client:latest localhost:5000/python-amqp-reliable-client:latest
+docker tag python-amqp-reliable-client:latest localhost:5000/python-amqp-reliable-client:latest
 docker push localhost:5000/python-amqp-reliable-client:latest
