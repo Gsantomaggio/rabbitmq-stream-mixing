d9855b87b6ced1615d042f2a8f9007208a3eb311
Gabriele Santomaggio <G.santomaggio@gmail.com>
2026-09-22 14:48:43 +0200

    add env
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- k8s_setup/deploy_go_amqp1.0.sh
+++ k8s_setup/deploy_go_amqp1.0.sh
@@ -1,5 +1,5 @@
 rm -rf rabbitmq-amqp-go-client
-git clone https://github.com/rabbitmq/rabbitmq-amqp-go-client.git .
+git clone https://github.com/rabbitmq/rabbitmq-amqp-go-client.git
 cd rabbitmq-amqp-go-client && make docker-build-reliable-example
 rm -rf rabbitmq-amqp-go-client
 docker tag go-amqp1.0-client:latest localhost:5000/go-amqp1.0-client:latest
