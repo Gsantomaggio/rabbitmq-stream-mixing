20e9d82f827cab8188123621b6d2df0e64f6e581
Gabriele Santomaggio <G.santomaggio@gmail.com>
2026-09-22 15:28:28 +0200

    add reliable.py example
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- k8s_setup/deploy_python_amqp_console.sh
+++ k8s_setup/deploy_python_amqp_console.sh
@@ -1,6 +1,6 @@
 rm -rf rabbitmq-amqp-python-client
 git clone --branch v2_main https://github.com/rabbitmq/rabbitmq-amqp-python-client.git
-docker build -f rabbitmq-amqp-python-client/docs/examples/docker/Dockerfile -t python-amqp-console-client rabbitmq-amqp-python-client
+docker build -f rabbitmq-amqp-python-client/docs/examples/docker/Dockerfile -t python-amqp-reliable-client rabbitmq-amqp-python-client
 rm -rf rabbitmq-amqp-python-client
-docker tag python-amqp-console-client:latest localhost:5000/python-amqp-console-client:latest
-docker push localhost:5000/python-amqp-console-client:latest
+docker tag python-amqp-console-client:latest localhost:5000/python-amqp-reliable-client:latest
+docker push localhost:5000/python-amqp-reliable-client:latest
